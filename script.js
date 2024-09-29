b9a41ef24bc414fca5b8619315a1b40655bf1b1b
Front-End Developer | Bootstrap, Tailwind CSS, JavaScript, and React.js | Crafting Responsive Web ApplicationsMuhammad Faisal <faisal032093@gmail.com>
2024-09-29 11:12:39 +0000

    adjust days and month functionality

--- script.js
+++ script.js
@@ -4,11 +4,11 @@ const resultYears = document.querySelector(".years");
 const resultMonths = document.querySelector(".months");
 const resultDays = document.querySelector(".days");
 
-const now = new Date();
-userDob.max = now.toISOString().split("T")[0];
+userDob.max = new Date().toISOString().split("T")[0];
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
+  const now = new Date();
   const dob = new Date(userDob.value);
 
   const currentDate = {
@@ -26,6 +26,22 @@ form.addEventListener("submit", (e) => {
   let ageYears = currentDate.year - dobDate.year;
   let ageMonths = currentDate.month - dobDate.month;
   let ageDays = currentDate.date - dobDate.date;
+
+  if (ageDays < 0) {
+    ageMonths--;
+    const daysInPreviousMonth = new Date(
+      currentDate.year,
+      currentDate.month - 1,
+      0
+    ).getDate();
+
+    ageDays += daysInPreviousMonth;
+  }
+
+  if (ageMonths < 0) {
+    ageYears--;
+    ageMonths += 12;
+  }
 
   resultYears.textContent = ageYears;
   resultMonths.textContent = ageMonths;
