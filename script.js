1caeb26c9303b17be7648d9d442a3f12734a2a53
Front-End Developer | Bootstrap, Tailwind CSS, JavaScript, and React.js | Crafting Responsive Web ApplicationsMuhammad Faisal <faisal032093@gmail.com>
2024-09-29 07:52:44 +0000

    add eventlisner on form

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-const form = document.querr
+const form = document.querySelector("form");
+const date = document.getElementById("date");
+const resultYears = document.querySelector(".years");
+const resultMonths = document.querySelector(".months");
+const resultDays = document.querySelector(".days");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const now = new Date();
+  const dob = new Date(date.value);
+
+  const currentDate = {
+    year: now.getFullYear(),
+    month: now.getMonth() + 1,
+    date: now.getDate(),
+  };
+
+  const dobDate = {
+    year: dob.getFullYear(),
+    month: dob.getMonth() + 1,
+    date: dob.getDate(),
+  };
+
+  let ageYears = currentDate.year - dobDate.year;
+  let ageMonths = currentDate.month - dobDate.month;
+  let ageDays = currentDate.date - dobDate.date;
+
+  resultYears.textContent = ageYears;
+  resultMonths.textContent = ageMonths;
+  resultDays.textContent = ageDays;
+});
